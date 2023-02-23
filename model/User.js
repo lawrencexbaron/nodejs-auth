a1d16b3fc5e45855b9eefdce8058af19a7300bcf
Lawrence <lawrencebaron027@gmail.com>
2023-02-23 11:00:41 -0700

    Added timestamp

--- model/User.js
+++ model/User.js
@@ -1,29 +1,32 @@
 // Mongoose model for User collection in MongoDB database (model\User.js) - see the code snippet below:
 const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    min: 6,
-    max: 255,
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      min: 6,
+      max: 255,
+    },
+    email: {
+      type: String,
+      required: true,
+      min: 6,
+      max: 255,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 6,
+      max: 1024,
+    },
+    date: {
+      type: Date,
+      default: Date.now,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    min: 6,
-    max: 255,
-  },
-  password: {
-    type: String,
-    required: true,
-    min: 6,
-    max: 1024,
-  },
-  date: {
-    type: Date,
-    default: Date.now,
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("User", UserSchema);
